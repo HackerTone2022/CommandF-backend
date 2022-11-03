ecf27e19e6e35231f06de025d836e0ddf42615cb
unavailableusername09 <legeno09133@gmail.com>
2022-11-03 12:01:59 +0900

    .
    .

---
+++ commandF/src/main/java/com/midas/ios/commandF/CommandFApplication.java
@@ -0,0 +1,13 @@
+package com.midas.ios.commandF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandFApplication.class, args);
+	}
+
+}
---
+++ commandF/src/test/java/com/midas/ios/commandF/CommandFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midas.ios.commandF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandFApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
